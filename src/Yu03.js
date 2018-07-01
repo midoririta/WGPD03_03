a07760df39cbdf54251b08e942ee185b8752081c
user <a0981327636@gmail.com>
2018-07-01 10:34:25 +0800

    20180701_01

--- src/Yu03.js
+++ src/Yu03.js
@@ -1,11 +1,45 @@
 
 var Yu03Layer = cc.Layer.extend({
+    isDrag: false,
     sprite:null,
     ctor:function () {
         this._super();
-
+        this.setupMouse();
 
         return true;
+    },
+
+    setupMouse: function () {
+        
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseDown: function (e) {
+
+                var target = e.getCurrentTarget();
+
+                target.isDrag = true;
+
+
+
+            },
+            onMouseUp: function (e) {
+                var target = e.getCurrentTarget();
+                target.isDrag = false;
+                //cc.log("Up");
+            },
+            onMouseMove: function (e) {
+                var target = e.getCurrentTarget();
+                if(target.isDrag){
+                    var x = e.getLocationX();
+                    var y = e.getLocationY();
+                    cc.log("x: "+ x + "y: " + y);
+                }
+                //cc.log("move");
+            },
+            onMouseScroll: function (e) {
+                //cc.log("scroll");
+            }
+        },this);
     }
 });
 
