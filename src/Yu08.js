c7931fc04b9450b960b171678b6f6d6718746204
user <a0981327636@gmail.com>
2018-07-01 16:23:39 +0800

    20180701_08game2

--- src/Yu08.js
+++ src/Yu08.js
@@ -1,19 +1,18 @@
 var Yu08Layer = cc.Layer.extend({
-    sprite:null,
-    bg: null,
-    man: null,
+    bg:null,
+    dx:10,
+    man:null,
     manFrame: new Array(4),
-    dx: 10,
     isRight: true,
     ctor:function () {
         this._super();
 
         this.bg = new cc.Sprite(res.bg_png);
-        this.bg.x = this.bg.width/2;
-        this.bg.y = cc.winSize.height/2;
+        this.bg.x = this.bg.width /2;
+        this.bg.y = cc.winSize.height /2;
         this.addChild(this.bg);
 
-        var frameCache = cc.SpriteFrameCache;
+        var frameCache = cc.spriteFrameCache;
         frameCache.addSpriteFrames(res.man_plist, res.man_png);
         var img37 = frameCache.getSpriteFrame("image37.png");
         var img38 = frameCache.getSpriteFrame("image38.png");
@@ -22,51 +21,47 @@ var Yu08Layer = cc.Layer.extend({
         this.manFrame = [img37,img38,img39,img40];
 
         this.man = new cc.Sprite(this.manFrame[0]);
-        this.man.x = cc.winSize.width/2;
-        this.man.y = cc.winSize.height/2 + 44;
+        this.man.x = cc.winSize.width / 2;
+        this.man.y = cc.winSize.height / 2 + 44;
         this.addChild(this.man);
-        this.man.runAction(cc.flipX(true));
+        this.man.runAction(cc.flipX(this.isRight));
+
 
         cc.eventManager.addListener({
             event: cc.EventListener.KEYBOARD,
             onKeyPressed: function (keyCode,event) {
                 var target = event.getCurrentTarget();
                 switch(keyCode){
-                    case 39:   //right
+                    case 39:    // right
                         target.man.runAction(cc.flipX(true));
                         target.goForward();
                         break;
-                    case 37:   //left
+                    case 37:    // left
                         target.man.runAction(cc.flipX(false));
                         target.goBack();
                         break;
                 }
             },
             onKeyReleased: function (keyCode,event) {
-
             }
-        },this);
+        }, this);
 
-        
+
+
         return true;
     },
 
     goForward: function () {
-
-        if(this.bg.x + this.bg.width/2 - this.dx >= cc.winSize.width){
+        if (this.bg.x + this.bg.width/2 - this.dx>= cc.winSize.width){
             this.bg.x -= this.dx;
-
         }
     },
+
     goBack: function () {
-        this.man.runAction(cc.flipX(false));
-
-        if(this.bg.x - this.bg.width/2 + this.dx <= 0){
+        if (this.bg.x - this.bg.width/2 + this.dx <= 0){
             this.bg.x += this.dx;
-
         }
     }
-
 });
 
 var Yu08Scene = cc.Scene.extend({
