1541c9eba3fd2b38cc8e823681a159d178b1fe7a
user <a0981327636@gmail.com>
2018-07-01 14:14:02 +0800

    20180701_05twoPicParent

--- src/Yu06.js
+++ src/Yu06.js
@@ -1,77 +1,74 @@
-
 var Yu06Layer = cc.Layer.extend({
-    sprite1: null,
-    sprite2: null,
-    isTouch1: false,
+    sprite1:null,
+    sprite2:null,
+    isTouch1:false,
     ctor:function () {
         this._super();
 
         this.sprite2 = new cc.Sprite(res.HelloWorld_png);
         this.sprite2.attr({
-            x: cc.winSize.width*1/ 2 +40,
-            y: cc.winSize.height/2 -20
+            x: cc.winSize.width * 1/ 2 + 40,
+            y: cc.winSize.height / 2 - 20
         });
         this.addChild(this.sprite2);
 
         this.sprite1 = new cc.Sprite(res.HelloWorld_png);
         this.sprite1.attr({
-            x: cc.winSize.width*1/2 -40,
-            y: cc.winSize.height/2 +20
+            x: cc.winSize.width * 1/ 2 - 40,
+            y: cc.winSize.height / 2 + 20
         });
         this.addChild(this.sprite1);
 
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseDown: function (e) {
+                var target = e.getCurrentTarget();
+                var parent = target.getParent();
 
-        this.setupMouse1(this);
-        this.setupMouse2(this);
+                var targetRect = cc.rect(
+                    target.x - target.width/2,
+                    target.y - target.height/2,
+                    target.width,
+                    target.height
+                );
+                var point = new cc.Point(e.getLocationX(), e.getLocationY());
+                if (cc.rectContainsPoint(targetRect,point)){
+                    parent.isTouch1 = true
+                    cc.log("touch 11");
+                }
+            },
+            onMouseUp: function (e) {
+                var target = e.getCurrentTarget();
+                var parent = target.getParent();
+                parent.isTouch1 = false;
 
+            }
+        },this.sprite1);
+
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseDown: function (e) {
+                var target = e.getCurrentTarget();
+                var parent = target.getParent();
+
+                var targetRect = cc.rect(
+                    target.x - target.width/2,
+                    target.y - target.height/2,
+                    target.width,
+                    target.height
+                );
+                var point = new cc.Point(e.getLocationX(), e.getLocationY());
+                if (cc.rectContainsPoint(targetRect,point)
+                    && !parent.isTouch1){
+                    cc.log("touch 22");
+                }
+            },
+        },this.sprite2);
 
         return true;
     },
-    setupMouse1: function (layer) {
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-
-            onMouseDown: function (e) {
-                var target = e.getCurrentTarget();
-                var targetRect = cc.rect(
-                    target.x - target.width/2,
-                    target.y - target.height/2,
-                    target.width,
-                    target.height
-                );
-                var point = new cc.Point(e.getLocationX(), e.getLocationY());
-                if(cc.rectContainsPoint(targetRect,point)){
-                    cc.log("touch1");
-                    layer.isToch1 = true;
 
 
-                }
-            },
-            onMouseUp: function () {
-                layer.isTouch1 = false;
-            },
-        },layer.sprite1);
-    },
-    setupMouse2: function (layer) {
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-
-            onMouseDown: function (e) {
-                var target = e.getCurrentTarget();
-                var targetRect = cc.rect(
-                    target.x - target.width/2,
-                    target.y - target.height/2,
-                    target.width,
-                    target.height
-                );
-                var point = new cc.Point(e.getLocationX(), e.getLocationY());
-                if(cc.rectContainsPoint(targetRect,point) && !layer.isToch1){
-                    cc.log("touch2");
-
-                }
-            },
-        },layer.sprite2);
-    }
 });
 
 var Yu06Scene = cc.Scene.extend({
@@ -81,4 +78,3 @@ var Yu06Scene = cc.Scene.extend({
         this.addChild(layer);
     }
 });
-
